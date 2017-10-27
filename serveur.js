7e1be96188acc184ca403e9af3fc225160480afd
SebastienBourdin <mailbourdinsebastien@gmail.com>
2017-10-27 14:25:13 +0200

    add iteration on objects

--- serveur.js
+++ serveur.js
@@ -23,7 +23,7 @@ fs.readFile('./frise.html', function (err, frise) {
 
 
         } else if (page == '/liste') {
-
+            var html_response;
             con.connect(function (err) {
                 if (err)
                     throw err;
@@ -32,31 +32,32 @@ fs.readFile('./frise.html', function (err, frise) {
                     if (err)
                         throw err;
                     console.log("Result: " + result);
+
+                    html_response = "<html><head><title>Liste des frises disponibles</title>"
+                            + "<meta charset='UTF-8'>"
+                            + "<script src='http://code.jquery.com/jquery-1.12.4.min.js'></script>"
+                            + "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>"
+                            + "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'>"
+                            + "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"
+                            + "<link rel='stylesheet' href='style.css' />"
+                            + "</head>"
+                            + "<h1>Liste des frises disponnibles</h1>"
+                            + "<table class='table'>"
+                            + "<thead>"
+                            + "<tr><th>Nom de la frise</th><th>Action</th></tr>"
+                            + "</thead>"
+                            + "<tbody>";
+                    result.forEach(function (item) {
+                        html_response = html_response + "<tr><td></td><td></td></tr>";
+                    });
+                    html_response = html_response + "</tbody>"
+                            + "</table>"
+                            + "</html>";
+                   
                 });
             });
-
             res.writeHead(200, {"Content-Type": "text/html"});
-            res.write("<html><head><title>Liste des frises disponibles</title>"
-                    + "<meta charset='UTF-8'>"
-                    + "<script src='http://code.jquery.com/jquery-1.12.4.min.js'></script>"
-                    + "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>"
-                    + "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'>"
-                    + "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"
-                    + "<link rel='stylesheet' href='style.css' />"
-                    + "</head>"
-
-                    + "<h1>Liste des frises disponnibles</h1>"
-
-            +"<table class='table'>"
-                +"<thead>"
-                    +"<tr><th>Nom de la frise</th><th>Action</th></tr>"
-                +"</thead>"
-                +"<tbody>"
-               
-                +"</tbody>"
-            +"</table>"
-
-                    + "</html>");
+            res.write(html_response);
         } else if (page == '/frise') {
             res.writeHead(200, {"Content-Type": "text/html"});
             res.write(frise);
