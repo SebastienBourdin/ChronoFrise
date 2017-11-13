9a3c6e9365dfcd4a3fedc3d5e658d175df6c2ddc
SebastienBourdin <mailbourdinsebastien@gmail.com>
2017-11-13 09:52:41 +0100

    Ajout du module EJS

--- serveur.js
+++ serveur.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var ejs = require('ejs');
 var mysql = require('mysql');
 
 fs.readFile('./frise.html', function (err, frise) {
@@ -32,28 +33,7 @@ fs.readFile('./frise.html', function (err, frise) {
                     if (err)
                         throw err;
                     console.log("Result: " + result);
-
-                    html_response = "<html><head><title>Liste des frises disponibles</title>"
-                            + "<meta charset='UTF-8'>"
-                            + "<script src='http://code.jquery.com/jquery-1.12.4.min.js'></script>"
-                            + "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>"
-                            + "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css'>"
-                            + "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>"
-                            + "<link rel='stylesheet' href='style.css' />"
-                            + "</head>"
-                            + "<h1>Liste des frises disponnibles</h1>"
-                            + "<table class='table'>"
-                            + "<thead>"
-                            + "<tr><th>Nom de la frise</th><th>Action</th></tr>"
-                            + "</thead>"
-                            + "<tbody>";
-                    result.forEach(function (item) {
-                        html_response = html_response + "<tr><td></td><td></td></tr>";
-                    });
-                    html_response = html_response + "</tbody>"
-                            + "</table>"
-                            + "</html>";
-
+                    html_response = ejs.render({url: './view/liste.ejs'},{result:result});
                     res.writeHead(200, {"Content-Type": "text/html"});
                     res.write(html_response);
                 });
